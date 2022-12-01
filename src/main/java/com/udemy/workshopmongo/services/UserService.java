6a1e526bbb5dd2f77ae6dcd051d5ae37c83df00c
José Alfredo <netoalfredo9@gmail.com>
2022-12-01 00:03:47 -0300

    Atualização de usuário com PUT

--- src/main/java/com/udemy/workshopmongo/services/UserService.java
+++ src/main/java/com/udemy/workshopmongo/services/UserService.java
@@ -33,7 +33,20 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = userRepository.findById(user.getId()).get();
+		updateData(newUser, user);
+		return userRepository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
+	
+	
 }
