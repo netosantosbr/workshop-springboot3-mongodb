d5fc252061dad003d3faa8227bf33e2abbe3f8ed
José Alfredo <netoalfredo9@gmail.com>
2022-12-01 01:16:19 -0300

    Consulta simples com query methods

--- src/main/java/com/udemy/workshopmongo/services/PostService.java
+++ src/main/java/com/udemy/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.udemy.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +19,7 @@ public class PostService {
 		return postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 	
+	public List<Post> findByTitle(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 }
